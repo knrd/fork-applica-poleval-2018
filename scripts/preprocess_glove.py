4708d1913b46b764591f98c0e649a911ade24d80
knrd <jvdswi@jadamspam.pl>
2019-02-21 20:58:07 +0100

    fixing scripts

--- scripts/preprocess_glove.py
+++ scripts/preprocess_glove.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+
+
 import gensim
 
 
@@ -6,3 +9,4 @@ from models import GLOVE, GLOVE_POLEVAL_GENSIM
 
 word_vectors = gensim.models.KeyedVectors.load_word2vec_format(GLOVE, binary=False)
 word_vectors.save(GLOVE_POLEVAL_GENSIM)
+print("Saved", GLOVE_POLEVAL_GENSIM)
